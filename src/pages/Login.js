ceb663426b1b086e9fe40911decf794cfd7781f0
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-10-29 16:05:08 -0300

    add validate functions

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,65 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onEmailchange = this.onEmailchange.bind(this);
+  }
+
+  onEmailchange({ value }) {
+    const simpleEmailRegex = /.+@.+\.com/;
+    const validEmail = simpleEmailRegex.test(value);
+    const inputPassword = document.getElementById('password').value;
+    if (inputPassword !== null) {
+      const passwordMinimunLength = 6;
+      const validPassword = inputPassword.length >= passwordMinimunLength;
+      this.enableButton(validEmail, validPassword);
+    }
+  }
+
+  onPasswordChange({ value }) {
+    const passwordMinimunLength = 6;
+    const validPassword = value.length >= passwordMinimunLength;
+    const inputEmail = document.getElementById('email').value;
+    if (inputEmail !== null) {
+      const simpleEmailRegex = /.+@.+\..+/;
+      const validEmail = simpleEmailRegex.test(inputEmail);
+      this.enableButton(validEmail, validPassword);
+    }
+  }
+
+  enableButton(validEmail, validPassword) {
+    if (validEmail && validPassword) {
+      document.getElementById('login-button').removeAttribute('disabled');
+    } else {
+      document.getElementById('login-button').setAttribute('disabled', 'true');
+    }
+  }
+
   render() {
     return (
       <div>
         <form className="login-page">
           <input
             type="email"
+            id="email"
             data-testid="email-input"
             placeholder="email"
+            onChange={ ({ target }) => this.onEmailchange(target) }
           />
           <input
             type="password"
+            id="password"
             data-testid="password-input"
             placeholder="password"
+            onChange={ ({ target }) => this.onPasswordChange(target) }
           />
-          <button type="button">Entrar</button>
+          <Link to="/carteira">
+            <button type="button" id="login-button" disabled>Entrar</button>
+          </Link>
         </form>
       </div>
     );
