07048dc068924fb49b450d141cedc7a63ffe4200
Daniel Jesus <dev.danieljs@gmail.com>
2020-10-29 15:41:18 -0300

    feat: providing the store to components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import store from './store';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
