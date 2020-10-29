3b7a7ba2d1e541f2985cc2cea1eef39dae48f061
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-10-29 17:04:06 -0300

    linter max length

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter store={store}>
+      <App />
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import reducer from '../reducers';
 
 const rootReducer = combineReducers({ reducer });
 
-const store = createStore(rootReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
