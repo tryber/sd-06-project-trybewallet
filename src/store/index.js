7b4ad95b185cd76bc0cdd1f14ac356555e5cfba1
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-10-29 17:15:27 -0300

    será que vai?

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from '../reducers';
 
-const store = createStore(rootReducer,
-  applyMiddleware(thunk),
+const store = createStore(rootReducer, applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
