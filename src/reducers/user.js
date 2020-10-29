fdc2c7c815dc3bb0e3de9cc7e2fc07e8008fdd53
Tiago Berwanger <berwangertiago@gmail.com>
2020-10-29 16:35:20 -0300

    setting provider, store and reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+// O que é o reducer? O reducer é uma função que retorna um novo estado baseado no type da action.
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case '':
+    return;
+  default:
+    return state;
+  }
+}
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,14 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+// O que é o reducer? O reducer é uma função que retorna um novo estado baseado no type da action.
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case '':
+    return;
+  default:
+    return state;
+  }
+}
