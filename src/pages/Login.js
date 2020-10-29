b23adaf945a10b84c941784ef9b5fc47e7865a32
Julia Porto <juportob@gmail.com>
2020-10-29 17:17:44 -0200

    Requirement 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { loginAction } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    };
+
+    this.submitEmail = this.submitEmail.bind(this);
+  };
+
+  submitEmail(e) {
+    e.preventDefault();
+    const { email } = this.state;
+    const { importedThunk } = this.props;
+
+    this.setState({
+      email: e.target.value
+    }, () => {
+      loginDispatch(email)
+    })
+  };
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <fieldset>
+            <input type="email" data-testid="email-input" placeholder="Email" />
+            <input type="password" data-testid="password-input" placeholder="Password" minLength="6" />
+          </fieldset>
+          <button type="button" onClick={this.submitEmail}>
+            Entrar
+          </button>
+        </form>
+      </div>
+    )
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  loginDispatch: (email) => dispatch(loginAction(email))
+};
+
+Login.propTypes = {
+  loginDispatch: propTypes.func.isRequired,
+};
 
 export default Login;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,23 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { loginAction, SAVE_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: []
+  }
+}
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case SAVE_EMAIL:
+      return {...state, user: {email: action.payload}};
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
