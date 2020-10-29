e857ffb5acd243c5036d3a9aef153b905eabfed3
isabellavjs <isabellavjs@gmail.com>
2020-10-29 16:06:08 -0300

    Render Login and Wallet

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-class Login extends React.Component {
+class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            value=""
+            data-testid="email-input"
+            placeholder="Nome"
+            onChange={ () => console.log('Nome') }
+            required
+          />
+          <input
+            type="text"
+            value=""
+            data-testid="password-input"
+            placeholder="Email"
+            onChange={ () => console.log('Nome') }
+            maxLength="6"
+          />
+          <button type="button"><Link to="/carteira">Entrar</Link></button>
+        </form>
+      </div>
+    );
   }
 }
 
