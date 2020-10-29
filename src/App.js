63996f59a497897c1f19ccb94b7b409e4b331fba
Isadora Koga <isadorakoga710@gmail.com>
2020-10-29 16:28:58 -0300

    Login page setting state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from '../src/pages/Login';
+import Wallet from '../src/pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />;
+      <Route path="/carteira" component={ Wallet } />;
+    </Switch>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,46 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.state= {
+      email: '',
+      password: '',
+    }
+  }
+
+  onClick() {
+    const { history } = this.props;
+    const link = history.location.pathname;
+    link.push('carteira');
+  }
+
+  onChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+          <label htmlFor="email">
+            Email:
+            <input data-testid="email-input" type="text" name="email" pattern="/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/g" required
+            onChange={this.onChange}
+          ></input>
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input data-testid="password-input" type="password" name="password" minLength="6" required
+            onChange={this.onChange}
+            ></input>
+          </label>
+          <button type="submit" id="button-login" disabled onClick={this.onClick}>
+          </button>
+      </div>
+    );
   }
 }
 
