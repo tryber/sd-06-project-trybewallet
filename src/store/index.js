627e44aa9be467edf7cde0caf5115192a244943f
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-10-29 16:57:25 -0300

    Trybewallet - get rich with it

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from '../reducers';
+
+const rootReducer = combineReducers({ reducer });
+
+const store = createStore(rootReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
