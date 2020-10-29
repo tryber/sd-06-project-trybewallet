39272193da9049542eb5d3b21d6a2f1e37a3d30f
CristianoNunes <cristianonunes99@gmail.com>
2020-10-29 19:53:17 -0300

    Requisito 1 - Fixbugs3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
 import Wallet from './pages/Wallet';
+import Login from './pages/Login';
 
 class App extends Component {
   render() {
