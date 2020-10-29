e0312ee8cd03dc63403af526e11ea71c05df3e43
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-10-29 18:48:47 -0300

    Login form, without checking for button blocking

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,50 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
-        Login
+        <h1>TrybeWallet</h1>
+        <form>
+          <label htmlFor="email">
+            E-mail:
+            <input
+              type="text"
+              name="email"
+              data-testid="email-input"
+              onChange={ this.handleChange }
+              value={ email }
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="text"
+              name="password"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+              value={ password }
+            />
+          </label>
+          <button type="button">Entrar</button>
+        </form>
       </div>
     );
   }
