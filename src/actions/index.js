568c00cf9c46fb444ad3d6d644f1f5b955d250cf
CristianoNunes <cristianonunes99@gmail.com>
2020-10-29 18:11:45 -0300

    Requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-// Coloque aqui suas actions
+const { act } = require("@testing-library/react");
+
+export const SAVE_USER = 'SAVE_USER';
+
+
+export const user = (email) => (
+  {
+    type: SAVE_USER,
+    email,
+  }
+);
+
+export const wallet = (ewallet) => (
+  {
+    type: SAVE_USER,
+  }
+);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,91 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { user } from '../actions';
+import { Link } from 'react-router-dom';
+import Wallet from '../img/wallet.png';
+
 
 class Login extends React.Component {
+  constructor(){
+    super();
+    this.validateEmail = this.validateEmail.bind(this);
+    this.validatePassword = this.validatePassword.bind(this);
+    this.state = {
+      email: '',
+      emailValidate: false,
+      passwordValidate: false,
+    }
+  }
+
+  validateEmail = (email)  => {
+    let emailValidate = false;
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(email.toLowerCase())){
+      emailValidate = true;
+    }
+    this.setState({
+      email,
+      emailValidate
+    });
+  }
+
+  validatePassword = (password) => {
+    let passwordValidate = false;
+    if(password.length >= 6){
+      passwordValidate = true;
+    }
+    this.setState({
+      passwordValidate
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, emailValidate, passwordValidate} = this.state;
+    const { loginUser } = this.props;
+
+    return (
+      <div className="containerLogin">
+        <div>
+          <img src={Wallet} alt="Wallet" width="100" height="100" />
+        </div>
+        <div>
+          <input
+            data-testid="email-input"
+            type="email"
+            name="email"
+            placeholder="Digite seu email"
+            onChange={ e => this.validateEmail(e.target.value) }
+          />
+        </div>
+        <div>
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+            onChange={ e => this.validatePassword(e.target.value) }
+          />
+        </div>
+        <div>
+          <Link to="/carteira">
+            <button
+              type="button"
+              name="btnEntrar"
+              disabled={ !(emailValidate && passwordValidate) }
+              onClick={ () => loginUser(email) }
+            >
+              Entrar
+            </button>
+          </Link>
+          
+        </div>
+        
+      </div>
+    )
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  loginUser: (email) => dispatch(user(email))
+})
 
-export default Login;
+export default connect(null, mapDispatchToProps)(Login);
