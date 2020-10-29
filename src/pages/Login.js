c58038f905a517a99093e84ed2fc758a8d3d6d0b
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-10-29 14:48:21 -0300

    add Login.js and Login.css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './Login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form className="login-page">
+          <input
+            type="email"
+            data-testid="email-input"
+            placeholder="email"
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="password"
+          />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
