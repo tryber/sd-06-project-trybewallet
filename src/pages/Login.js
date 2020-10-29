32f8a85a7038a66993c0309be5dafbe9679a2840
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-10-29 16:27:44 -0300

    Css - bootstrap lint3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
-import trybewallet from './trybewallet'
+import trybewallet from './trybewallet.png';
 
 class Login extends React.Component {
   render() {
