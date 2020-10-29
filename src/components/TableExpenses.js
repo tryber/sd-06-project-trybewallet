c14bbce6e7e7c46a3daa0b87d76d60e9bbad96c8
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-29 18:14:04 -0300

    feat: create a expenses table

---
+++ src/components/TableExpenses.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class TableExpenses extends Component {
+  render() {
+    const headerTable = [
+      'Descrição',
+      'Tag',
+      'Método de pagamento',
+      'Valor',
+      'Moeda',
+      'Câmbio utilizado',
+      'Valor convertido',
+      'Moeda de conversão'];
+
+    return (
+      <div>
+        {headerTable.map((item, index) => (
+          <span key={ index }>
+            {item}
+          </span>))}
+      </div>
+    );
+  }
+}
+
+export default TableExpenses;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import ExpenseForm from '../components/ExpenseForm';
 import Header from '../components/Header';
+import TableExpenses from '../components/TableExpenses';
 
 class Wallet extends Component {
   render() {
     return (
       <div>
         <Header />
+        <ExpenseForm />
+        <TableExpenses />
       </div>
     );
   }
