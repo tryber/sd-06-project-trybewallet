2cff9dec8582546ffc30fcc37c49457d7b2bd1ba
Tiago Berwanger <berwangertiago@gmail.com>
2020-10-29 16:55:57 -0300

    setting first action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
 // Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+export default login = (email) => ({
+  type: LOGIN,
+  email,
+});
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,3 +1,4 @@
+import { LOGIN } from '../actions';
 // Esse reducer será responsável por tratar as informações da pessoa usuária
 const INITIAL_STATE = {
   email: '',
@@ -5,8 +6,8 @@ const INITIAL_STATE = {
 // O que é o reducer? O reducer é uma função que retorna um novo estado baseado no type da action.
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
-  case '':
-    return;
+  case LOGIN:
+    return { ...state, email: action.email };
   default:
     return state;
   }
