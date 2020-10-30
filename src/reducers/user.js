355421f01fa28b481473f5d0bbade15e67f560f1
Micaeli Teixeira <luime6@gmail.com>
2020-10-30 20:25:30 -0300

    Implementing index.js and user.js from src/reducers.

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,19 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+function userEmail(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case EMAIL:
+    return { ...state, email: action.value };
+  default:
+    return state;
+  }
+}
+
+export default userEmail;
