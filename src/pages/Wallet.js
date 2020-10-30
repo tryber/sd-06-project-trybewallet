1204eb59c9c47793c1318216bd2830652b2d5f57
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-10-30 19:24:32 -0300

    fetching state from store in wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { userRegister } from '../actions';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <div data-testid="email-field">{email}</div>
+        <div data-testid="total-field">0</div>
+        <div data-testid="header-currency-field">BRL</div>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
