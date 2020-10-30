897129c22264f9ba42f3046a002879e783796766
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-10-30 18:22:21 -0300

    store set up, login page added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
-// Coloque aqui suas actions
+export const USER_REGISTER = 'USER_REGISTER';
+export const WALLET = 'WALLET';
+
+export const userRegister = (email) => ({
+  type: USER_REGISTER,
+  email,
+});
+
+export const wallet = () => ({
+  type: WALLET,
+});
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { WALLET } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case WALLET:
+    return state;
+  default:
+    return state;
+  }
+}
