82373f993b8070c46c2e207761f76933b9bb842d
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-10-30 16:36:56 -0300

    more issues

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -3,15 +3,15 @@ import { WALLET_CHANGES } from '../actions';
 const WALLET_INITIAL_STATE = {
   currencies: [],
   expenses: [],
-}
+};
 
 const wallet = (state = WALLET_INITIAL_STATE, action) => {
   switch (action.type) {
   case WALLET_CHANGES:
     return ({
       ...state,
-      currencies: [...state.currencies, action.payload.currencies ],
-      expenses: [ ...state.expenses, action.payload.expenses ]
+      currencies: [...state.currencies, action.payload.currencies],
+      expenses: [...state.expenses, action.payload.expenses],
     });
   default:
     return state;
