bf2f04613b48d7ebf4ac65dbace474c7303577ad
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-10-30 17:05:32 -0300

    changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { userEmail, userExpenses } = this.props;
+
+    return (
+      <header>
+        <span data-testid="email-field">
+          User:
+          { userEmail }
+        </span>
+        <span data-testid="total-field">
+          Total Spends:
+          { userExpenses.length > 0 ? userExpenses.length : 0 }  
+        </span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+  userExpenses: state.wallet.expenses,
+});
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+  userExpenses: PropTypes.arrayOf().isRequired, 
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends Component {
   render() {
-    const { userEmail } = this.props;
-
     return (
       <section>
-        <header>
-          <div className="user-data">
-            <span data-testid="email-field">
-              User:
-              { userEmail }
-            </span>
-            <span data-testid="total-field">Total Spends: 0</span>
-            <span data-testid="header-currency-field">BRL</span>
-          </div>
-        </header>
+        <Header />
       </section>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  userEmail: state.user.email,
-});
-
-Wallet.propTypes = {
-  userEmail: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
