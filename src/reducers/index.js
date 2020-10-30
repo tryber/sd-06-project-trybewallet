68d93c9dde0b3756d0b3538e0d1e3c021acdd0b6
Alvaro Neto <alvaroshn@gmail.com>
2020-10-30 00:34:36 -0300

    Req 1 teste 2

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import user from './user';
-import wallet from './wallet';
 
-//  const rootReducers = combineReducers({ user, wallet });
 const rootReducers = combineReducers({ user });
 console.log(rootReducers);
 export default rootReducers;
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-// const store = createStore((rootReducer, applyMiddleware(thunk)));
-const store = createStore((rootReducer));
+const store = createStore((rootReducer, applyMiddleware(thunk)));
 
 export default store;
