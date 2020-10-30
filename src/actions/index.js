9be72238fb1de677f840013ed4908eec65873e34
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-10-30 14:07:39 -0300

    vai agora?

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,7 @@
 export const USER_EMAIL = 'USER_EMAIL';
 export const WALLET_CHANGES = 'WALLET_CHANGES';
 
-export const addUserEmail = email => {
-  return ({
-    type: USER_EMAIL,
-    payload: email,
-  });
-};
+export const addUserEmail = (email) => ({
+  type: USER_EMAIL,
+  payload: email,
+});
