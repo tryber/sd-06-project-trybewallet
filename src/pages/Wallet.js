ac0a6e5d4e7792cd921ddea76a0d7e6054f676cb
Alessandra <itsalematos@hotmail.com>
2020-10-30 15:28:42 -0400

    fixing lint 2.0 - 2

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
-    <header>
-      <h2 data-testid="email-field">{email}</h2>
-      <p data-testid="total-field">0</p>
-      <p data-testid="header-currency-field">BRL</p>
-    </header>
+      <header>
+        <h2 data-testid="email-field">{email}</h2>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  email: state.user.email
-})
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: propTypes.string.isRequired,
+}
 
 export default connect(mapStateToProps)(Wallet);
