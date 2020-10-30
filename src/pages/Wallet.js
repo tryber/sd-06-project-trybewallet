640fa6d72d7b352b4f8ec2f4011d5e12a64777c9
Alvaro Neto <alvaroshn@gmail.com>
2020-10-30 00:44:18 -0300

    req 1 teste 4

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
@@ -7,5 +7,5 @@ class Wallet extends React.Component {
   }
 }
 
-export default connect(null, null)(Wallet);
-// export default Wallet;
+// export default connect(null, null)(Wallet);
+export default Wallet;
