8bc90cff54fa6642cdb95e1cd7c00dc992c313c5
FlavioSugano <flavio.sugano@gmail.com>
2020-10-30 01:38:27 -0300

    Requisito 1: pagina de login

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,16 +3,20 @@ import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        TrybeWallet
+      </div>
+    );
   }
 }
 
 export default Wallet;
 
 // const mapStateToProps = (state) => ({
-
+//   nomeRandom: state.wallet.hello,
 // });
 
 // export default connect(
 //   mapStateToProps,
-// )(Wallet)
+// )(Wallet);
