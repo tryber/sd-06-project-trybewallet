16c60dceb2498e8176db64db60ca2372d4bb757e
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-10-30 13:51:33 -0300

    Trabalhando requisito 4 - subindo para não correr risco

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -12,6 +12,46 @@ class Wallet extends React.Component {
           <p data-testid="total-field">0</p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
+        <form>
+          <label htmlFor="valor-despesa">
+            Valor:
+            <input
+              type="number"
+              data-testid="value-input"
+            />
+          </label>
+          <label htmlFor="descricao-despesa">
+            Descrição:
+            <input
+              type="number"
+              data-testid="description-input"
+            />
+          </label>
+          <select data-testid="currency-input">
+            <option value="Escolha">Currency</option>
+            <option value="usd">USD</option>
+            <option value="cad">CAD</option>
+            <option value="eur">EUR</option>
+          </select>
+          <select data-testid="method-input">
+            <option value="dinheiro">Dinheiro</option>
+            <option value="credito">Cartão de crédito</option>
+            <option value="debito">Cartão de débito</option>
+          </select>
+          <select data-testid="tag-input">
+            <option value="alimentacao">Alimentação</option>
+            <option value="lazer">Lazer</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="saude">Saúde</option>
+          </select>
+          <button
+            type="button"
+            data-testid="delete-btn"
+            // onClick={}
+          >
+            Delete
+          </button>
+        </form>
       </div>);
   }
 }
