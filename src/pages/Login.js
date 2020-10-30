7f7fe0b5ff33b9d2366473b934cedeccacd90dba
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-10-30 09:18:37 -0300

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import trybewallet from './trybewallet.png';
