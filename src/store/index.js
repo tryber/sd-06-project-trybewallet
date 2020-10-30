60b3132de06d8fe9164a93b7299f50201becd00b
DekoPossas <andrepossaster@gmail.com>
2020-10-30 19:01:31 -0300

    cirei meu store

---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+const { combineReducers } = require("redux");
+import { createStore, combineReducers } from 'redux';
+
+const store = createStore(combineReducers({ reducer }))
+
+export default store;
