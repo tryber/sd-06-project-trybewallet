0594cbb2097d8c28b4aa500ac8d4c5841520c4e9
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-10-30 14:27:30 -0300

    add addRecord reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+function addRecord(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'addEmail':
+      return {
+        ...state,
+        user: action.payload.user.email,
+      };
+    default:
+      return state;
+  }
+}
+
+export default addRecord;
