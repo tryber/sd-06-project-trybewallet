d88b688785bace310e1c5d71048253160aa847ee
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-10-30 00:08:06 -0300

    :sparkles: Implementation of Requirements 01 and 02

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userLogin } from '../actions';
 
 class Login extends React.Component {
+  componentDidMount() {
+    const btnEnviar = document.getElementsByTagName('button')[0];
+
+    btnEnviar.disabled = true;
+  }
+
   render() {
-    return <div>Login</div>;
+    const { userEmail } = this.props;
+
+    function validateInputs() {
+      const email = document.getElementsByTagName('input')[0].value;
+      const password = document.getElementsByTagName('input')[1].value;
+      const btnEnviar = document.getElementsByTagName('button')[0];
+      const outOfString = -1;
+      const pwdSize = 6;
+
+      if (!email || email.indexOf('@') === outOfString
+        || email.indexOf('.com') === outOfString
+        || password.length < pwdSize) {
+        btnEnviar.disabled = true;
+      } else {
+        btnEnviar.disabled = false;
+      }
+    }
+
+    function handleButton() {
+      const email = document.getElementsByTagName('input')[0].value;
+
+      userEmail(email);
+    }
+
+    return (
+      <div>
+        <form>
+          <input
+            type="email"
+            data-testid="email-input"
+            onChange={ validateInputs }
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            onChange={ validateInputs }
+          />
+        </form>
+        <Link to="/carteira">
+          <button type="button" onClick={ handleButton }>Entrar</button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  userEmail: (e) => dispatch(userLogin(e)),
+});
+
+Login.propTypes = {
+  userEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
