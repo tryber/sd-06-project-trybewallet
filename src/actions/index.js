23394f1303158d8be04971a5a30062281836c720
Alessandra <itsalematos@hotmail.com>
2020-10-30 18:39:13 -0400

    updating

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
+import fetchAPI from '../services/mockAPI';
 export const EMAIL = 'EMAIL';
+export const CURRENCY = 'CURRENCY';
 
 export const loginAction = (payload) => ({
   type: EMAIL,
   payload,
 });
+
+// export const fetchAction = (payload) => ({
+//   type: CURRENCY,
+//   payload
+// })
+
+// export const currencyThunk = () => {
+//   return async (dispatch) => {
+//     const request = fetchAPI();
+//     return dispatch(fetchAction(request));
+//   }
+// }
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { CURRENCY } from '../actions';
+
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: []
+  },
+}
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case CURRENCY:
+      return { ...state, currencies: action.payload };
+    default:
+      return state;
+  }
+}
