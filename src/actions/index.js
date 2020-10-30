db0cc5c4db026f75a0d7094f464419d7bf402f9d
danieleperse <danieleperse3@gmail.com>
2020-10-30 18:17:53 -0300

    criada pasta componente para separar os itens da pagina carteira e add api disponibilizda pelo professor icaro na pasta services

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,14 @@
 // definição: objeto com propriedade type e qq outro valor q eu queria passar
 export const LOGIN = 'LOGIN';
-// export const WALLET = 'WALLET';
+export const WALLET = 'WALLET';
 
 export const login = (email) => ({
   type: LOGIN,
   email,
 });
 
-// export const wallet = (currencies, expenses) => ({
-//   type: WALLET,
-//   currencies,
-//   expenses
-// });
+export const wallet = (currencies, expenses) => ({
+  type: WALLET,
+  currencies: [],
+  expenses: []
+});
---
+++ src/components/Form.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Form extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <form>
+        <label
+          htmlFor="valorDespesa"
+        >
+          Valor da Despesa
+          <input
+            data-testid="value-input"
+            type="text"
+            id="valorDespesa"
+          />
+        </label>
+        <label
+          htmlFor="descricaoDespesa"
+        >
+          Descrição da despesa
+          <input
+            data-testid="description-input"
+            type="text"
+            id="descricaoDespesa"
+          />
+        </label>
+        <label
+          htmlFor="moedaCorrente"
+        >
+          <select
+            data-testid="currency-input"
+            name="moedaCorrente"
+            id="moedaCorrente"
+          >
+            <option data-testid="" value="">{}</option>
+          </select>
+        </label>
+        <label
+          htmlFor="metodoPgto"
+        >
+          <select
+            data-testid="method-input"
+            name="metodoPgto"
+            id="metodoPgto"
+          >
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+          </select>
+        </label>
+        <label
+          htmlFor="categoriaDespesa"
+        >
+          <select
+            data-testid="tag-input"
+            name="categoriaDespesa"
+            id="categoriaDespesa"
+          >
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+        <button
+          type="submit"
+        >
+          Adicionar despesa
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
