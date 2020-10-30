af63ba3a07c3cbd6b6f65e793aaf27eaeb1fd717
Julia Porto <juportob@gmail.com>
2020-10-30 14:13:04 -0200

    Requirement 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,41 +1,68 @@
 import React from 'react';
-// import { loginAction } from '../actions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginAction } from '../actions';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
+      password: '',
+      isDisabled: true,
     };
 
-    this.submitEmail = this.submitEmail.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.submitLogin = this.submitLogin.bind(this);
   }
 
-  submitEmail(e) {
-    e.preventDefault();
-    const { email } = this.state;
-
+  handleInput(e) {
     this.setState({
-      email: e.target.value,
+      [e.target.name]: e.target.value,
     }, () => {
-      loginDispatch(email);
+      const { email, password } = this.state;
+      const emailVerify = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/.test(email);
+      const passwordSize = 6;
+      if(emailVerify && password.length >= passwordSize) {
+        this.setState({ isDisabled: false });
+      } else {
+        this.setState({ isDisabled: true });
+      }
     });
   }
 
+  submitLogin(e) {
+    e.preventDefault();
+    const { history, loginDispatch } = this.props;
+    const { email } = this.state;
+    loginDispatch(email);
+    history.push('carteira');
+  }
+
   render() {
+    const { email, password, isDisabled } = this.state;
     return (
       <div>
-        <form>
+        <form onSubmit={ this.submitLogin }>
           <fieldset>
-            <input type="email" data-testid="email-input" placeholder="Email" />
+            <input
+              type="email"
+              data-testid="email-input"
+              placeholder="Email"
+              name="email"
+              value={ email }
+              onChange={ this.handleInput }
+            />
             <input
               type="password"
               data-testid="password-input"
               placeholder="Password"
-              minLength="6"
+              name="password"
+              value={ password }
+              onChange={ this.handleInput }
             />
           </fieldset>
-          <button type="button" onClick={ this.submitEmail }>
+          <button type="submit" disabled={ isDisabled }>
             Entrar
           </button>
         </form>
@@ -44,12 +71,12 @@ class Login extends React.Component {
   }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//   loginDispatch: (email) => dispatch(loginAction(email))
-// };
+const mapDispatchToProps = (dispatch) => ({
+  loginDispatch: (email) => dispatch(loginAction(email))
+});
 
-// Login.propTypes = {
-//   loginDispatch: propTypes.func.isRequired,
-// };
+Login.propTypes = {
+  loginDispatch: PropTypes.func.isRequired,
+};
 
-export default Login;
+export default connect(null, mapDispatchToProps)(Login);
