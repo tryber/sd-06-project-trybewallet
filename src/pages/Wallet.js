9c2b771d370e63fdfad226adb7e330bbd1a0d1a4
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-10-30 17:42:43 -0300

    Added value and description inputs in expense form

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import WalletHeader from '../wallet_components/WalletHeader';
+import ExpenseForm from '../wallet_components/ExpenseForm';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <WalletHeader />
+        <ExpenseForm />
       </div>
     );
   }
--- src/wallet_components/ExpenseForm.js
+++ src/wallet_components/ExpenseForm.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import { addExpense } from '../actions';
 
 class ExpenseForm extends React.Component {
   constructor() {
     super();
     this.state = {
       expense: 0,
-      currency: 'BRL',
-      paymentMethod: 'Dinheiro',
-      tag: '',
+      // currency: 'BRL',
+      // paymentMethod: 'Dinheiro',
+      // tag: '',
       description: '',
     };
     this.handleChange = this.handleChange.bind(this);
@@ -23,7 +24,7 @@ class ExpenseForm extends React.Component {
   }
 
   render() {
-    const { expense } = this.state;
+    const { expense, description } = this.state;
     return (
       <form>
         <label htmlFor="expense">
@@ -33,6 +34,17 @@ class ExpenseForm extends React.Component {
             type="number"
             data-testid="value-input"
             value={ expense }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="description">
+          Valor:
+          <input
+            name="description"
+            type="text"
+            data-testid="description-input"
+            value={ description }
+            onChange={ this.handleChange }
           />
         </label>
       </form>
@@ -45,7 +57,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  expense: (object) => dispatch(addExpense(object)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExpenseForm);
