bc6c79c9daf1071e39381fb963184ad04b561431
LeandroParisi <lepcbelisario@gmail.com>
2020-10-30 13:32:52 -0300

    Requisito 4 e 5 feitos - com mock - API CAIU 4

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,8 +5,8 @@ import wallet from './wallet';
 // import { initialStateWithExpenses } from '../tests/mockData';
 
 const rootReducers = combineReducers({ user, wallet });
-const composedThunk = compose(applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// const composedThunk = compose(applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 // export const store = createStore(rootReducers, initialStateWithExpenses, composedThunk);
 
