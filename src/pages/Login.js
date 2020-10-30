57d0154c0037a07eaf144fa69bf183ccb418423a
jederson <jmacedo.ti@gmail.com>
2020-10-30 15:13:06 -0300

    Primeiros requisitos, 01 e 02

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,93 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import propType from 'prop-types';
+import { Link } from 'react-router-dom';
+import login from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      email: '',
+      // password: '',
+      isEmail: true,
+      isPassword: true,
+    };
+  }
+
+  handleChangeEmail({ target }) {
+    const reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,64}/;
+    const email = target.value;
+    const boolean = email.match(reg);
+
+    if (boolean !== null) {
+      this.setState({ isEmail: false, email });
+    } else {
+      this.setState({ isEmail: true, email });
+    }
+  }
+
+  handleChangePassword({ target }) {
+    const password = target.value;
+    const passwordLength = 6;
+
+    if (password.length >= passwordLength) {
+      this.setState({ isPassword: false });
+    } else {
+      this.setState({ isPassword: true });
+    }
+  }
+
+  handleClick() {
+    const { addEmail } = this.props;
+    const { email } = this.state;
+    addEmail(email);
+  }
+
   render() {
-    return <div>Login</div>;
+    const { isEmail, isPassword } = this.state;
+    const validate = isEmail + isPassword;
+
+    return (
+      <div>
+        <h1>Digite email e senha:</h1>
+        <input
+          type="email"
+          data-testid="email-input"
+          onChange={ this.handleChangeEmail }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          onChange={ this.handleChangePassword }
+        />
+        <Link
+          to="/carteira"
+        >
+          <button
+            type="button"
+            disabled={ validate }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  addEmail: (email) => dispatch(login(email)),
+});
+
+Login.propTypes = {
+  addEmail: propType.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+// import thunk from 'redux-thunk';
+import reducer from '../reducers';
+
+// const store = createStore(reducer,
+//   compose(applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// ));
+
+const store = createStore(reducer);
+
+export default store;
