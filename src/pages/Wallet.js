2705a138816542b02d00bb97b7f114a0f2d561b2
danieleperse <danieleperse3@gmail.com>
2020-10-30 10:05:53 -0300

    elaborando req 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userReducer } = this.props;
+    return (
+      <div>
+        <span>E-mail</span>
+        <span data-testid="email-field">{ userReducer.email }</span>
+        <span>Despesa Total</span>
+        <span data-testid="total-field">0</span>
+        <span>Câmbio utilizado</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (StateDaStore) => ({
+  userReducer: StateDaStore,
+});
+
+Wallet.propTypes = {
+  userReducer: PropTypes.string.isRequired,
+};
+
+export default connect(
+  mapStateToProps,
+  null,
+)(Wallet);
