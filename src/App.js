457db1696a64681438e959c8241b689125898b23
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-31 09:25:14 -0300

    cria as rotas da aplicação home('/') como Login - acesso do usuário '/carteira' com Wallet - interação com a aplicação e página não encontrada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+import { Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import NotFound from './pages/NotFound';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+        <Route component={ NotFound } />
+      </Switch>
+    );
+  }
 }
 
 export default App;
