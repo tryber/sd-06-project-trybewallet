f05d4254f6ea8cf1c06b47c128578af2f929e0bb
HCaetano <hrcgeo@gmail.com>
2020-10-31 16:12:11 -0300

    Initial commit

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { loginAction } from '../actions';
 
 class Wallet extends React.Component {
+  constructor() {
+    super()
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { loginAction } = this.props
+    loginAction('email')
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { nomequalquer } = this.props
+
+    return <div onClick={this.handleClick}>TrybeWallet {nomequalquer}</div>;
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  nomequalquer: state.wallet.helloWorld,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loginAction: (email) => dispatch(loginAction(email)) 
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
