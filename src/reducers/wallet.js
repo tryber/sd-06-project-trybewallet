4975a21986893994817cafa4f37c41491c53970c
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-31 07:44:45 -0300

    [e. react-redux] Reducers wallet.js estrutura básica do reducer sem qualquer implementação. O reducer recebe o state e a action, retornando um novo state baseado no type da action

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,13 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+export default function usosCarteira(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case '':
+    return;
+  default:
+    return state;
+  }
+}
