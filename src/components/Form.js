79dd462a91f1c1a31ea44d81df314ff26851469f
ricardorosa-dev <ricardorosa@gmail.com>
2020-10-31 19:40:55 -0300

    esLint

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 function Form() {
   const moedas = ['USD', 'CAD', 'EUR', 'GBP', 'ARS', 'BTC',
@@ -13,12 +12,11 @@ function Form() {
       <label htmlFor="moeda">
         Moeda:
         <select id="moeda" data-testid="currency-input">
-          { moedas.map((moeda, index) => <option
-            key={ index }
-            data-testid={ moeda }
-            value={ moeda }>
-            { moeda }
-          </option>) }
+          { moedas.map((moeda, index) => (
+            <option key={ index } data-testid={ moeda } value={ moeda }>
+              { moeda }
+            </option>))
+          }
         </select>
       </label>
       <label htmlFor="descricao">
