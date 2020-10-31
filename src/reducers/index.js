7387e08338eb7ce6a9d0d2ac6c5d54aa60fe2152
DekoPossas <andrepossaster@gmail.com>
2020-10-31 20:09:33 -0300

    criei e exportei o rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,10 @@
-import user from './user';
-import wallet from './wallet';
+import { combineReducers } from 'redux';
+import userReducer from './user';
+import walletReducer from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
-const INITIAL_STATE = {
-  user: {
-    email: '',
-  },
-  wallet: {
-    currencies: [],
-    expenses: []
-  }
-}
+const rootReducer = combineReducers({ userReducer, WalletReducer});
 
-export default { user, wallet };
+export default rootReducer;
