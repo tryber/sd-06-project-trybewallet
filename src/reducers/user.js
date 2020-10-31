781d18392c9ec019eab167c9910ca9ac5a63bda5
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-31 08:01:05 -0300

    [e. react-redux] Actions e Reducers importa o LOGIN da action para o reducer user.js, além de retornar caso esse type seja acionado, um novo estado.

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,11 +1,13 @@
+import { LOGIN } from '../actions';
+
 const INITIAL_STATE = {
   email: '',
 };
 
 export default function loginUsuario(state = INITIAL_STATE, action) {
   switch (action.type) {
-  case '':
-    return;
+  case LOGIN:
+    return { ...state, email: action.email };
   default:
     return state;
   }
