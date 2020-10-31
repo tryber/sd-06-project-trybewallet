37c388f9b1e8fcddedd57d947126d6cabf8c90c2
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-31 07:38:47 -0300

    [e. react-redux] Reducers user.js estrutura básica do reducer sem qualquer implementação. O reducer recebe o state e a action, retornando um novo state baseado no type da action

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,12 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+export default function loginUsuario(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case '':
+    return;
+  default:
+    return state;
+  }
+}
