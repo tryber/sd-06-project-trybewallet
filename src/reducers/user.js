84887f5ee432bf2713b95bc2f2033262a05f2fe1
Leandro <leandro.feitoza8018@gmail.com>
2020-10-31 19:01:13 -0300

    Feito o 1 requisito teste local passando

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,16 +1,11 @@
 const INITIAL_STATE_USER = {
-  // user: {
   email: '',
-  // },
 };
 
 function userReducer(state = INITIAL_STATE_USER, action) {
-  console.log(action);
-  console.log(action.value);
-
   switch (action.type) {
   case 'LOGIN':
-    return { ...state, ...action.value  };
+    return { ...state, ...action.value };
   default:
     return state;
   }
