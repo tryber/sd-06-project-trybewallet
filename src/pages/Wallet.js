c8be25f7cff4737bcd405f8d5761ce13680c3fb1
ricardorosa-dev <ricardorosa@gmail.com>
2020-10-31 17:48:57 -0300

    esLint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import '../index.css'
+import '../index.css';
 import trybeLogo from '../img/trybe-logo.png';
 
-function Wallet({email}) {
-  // Vai mandar?
+function Wallet({ email }) {
   return (
-  <header>
-    <img src={trybeLogo} />
-    <div>
-      <span data-testid="email-field">E-mail: {email}</span>
-      <span data-testid="total-field">Despesa Total: R$ 0,00</span> 
-      <span data-testid="header-currency-field">BRL</span>
-    </div>
-  </header>
-  )
+    <header>
+      <img src={ trybeLogo } alt='Trybe logo' />
+      <div>
+        <span data-testid="email-field">E-mail: 
+          {email}
+        </span>
+        <span data-testid="total-field">Despesa Total: R$ 0,00</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </div>
+    </header>
+  );
 }
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
-})
+});
 
 export default connect(mapStateToProps)(Wallet);
