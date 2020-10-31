dda091a0a836339a6d5821abc29e0b59bca2c422
DekoPossas <andrepossaster@gmail.com>
2020-10-31 14:05:19 -0300

    retornei para o que estava pois eu havia confundido

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,17 @@
-// import user from './user';
-// import wallet from './wallet';
-import createStore from 'redux';
+import user from './user';
+import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
-const store = createStore();
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: []
+  }
+}
 
-export default store;
+export default { user, wallet };
