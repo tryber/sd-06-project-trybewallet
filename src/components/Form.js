181732e7d08b663474ce0f8586c7e2b97bf2e0f9
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-10-31 14:49:28 -0300

    reder dynamically options for select on Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,52 @@
 import React from 'react';
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.tagSelectMount = this.tagSelectMount.bind(this);
+    this.paymentSelectMount = this.paymentSelectMount.bind(this);
+    this.currencyMount = this.currencyMount.bind(this);
+  }
+
+  componentDidMount() {
+    this.paymentSelectMount();
+    this.tagSelectMount();
+    this.currencyMount();
+  }
+
+  async currencyMount() {
+    const currencyFetch = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const currencyJson = await currencyFetch.json();
+    const currencies = Object.keys(currencyJson).filter((cur) => cur !== 'USDT');
+    currencies.forEach((cur) => {
+      const option = document.createElement('option');
+      option.innerText = cur;
+      option.setAttribute('data-testid', cur);
+      const select = document.querySelectorAll('select')[0];
+      select.appendChild(option);
+    });
+  }
+
+  paymentSelectMount() {
+    const paymentMethod = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+    paymentMethod.forEach((payment) => {
+      const option = document.createElement('option');
+      option.innerText = payment;
+      const select = document.querySelectorAll('select')[1];
+      select.appendChild(option);
+    });
+  }
+
+  tagSelectMount() {
+    const tags = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+    tags.forEach((tag) => {
+      const option = document.createElement('option');
+      option.innerText = tag;
+      const select = document.querySelectorAll('select')[2];
+      select.appendChild(option);
+    });
+  }
+
   render() {
     return (
       <div>
@@ -8,13 +54,13 @@ class Form extends React.Component {
           <input data-testid="value-input" type="number" />
           <input data-testid="description-input" type="text" />
           <select data-testid="currency-input">
-            <option>USD</option>
+            {' '}
           </select>
           <select data-testid="method-input">
-            <option>Dinheiro</option>
+            {' '}
           </select>
           <select data-testid="tag-input">
-            <option>Lazer</option>
+            {' '}
           </select>
           <button type="button">Adicionar Despesa</button>
         </form>
