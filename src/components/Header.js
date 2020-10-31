7f6e7172290176b10f88fed22cba2222eed3f84b
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-30 23:09:25 -0300

    feat: add email using the global state with redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
       <header>
         <span data-testid="email-field">
           Email:
-          {}
+          {email}
         </span>
         <span data-testid="total-field">
           Despesa total:
@@ -20,4 +23,12 @@ class Header extends Component {
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
