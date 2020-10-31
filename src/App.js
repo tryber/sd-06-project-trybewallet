934d61cd71aa3705d5d3cd4ab87951d17e7989e3
Leandro <leandro.feitoza8018@gmail.com>
2020-10-31 19:00:23 -0300

    Feito o 1 requisito teste local passando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
-
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/carteira" component={ Wallet } />
