e9795812574551643be21e2e3b48c93c0fa9d1f6
ricardorosa-dev <ricardorosa@gmail.com>
2020-10-31 19:04:03 -0300

    Parte do requisito 4. Antes de fazer a requisição.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 function Form() {
+  const moedas = ['USD', 'CAD', 'EUR', 'GBP', 'ARS', 'BTC', 'LTC', 'JPY', 'CHF', 'AUD', 'CNY', 'ILS', 'ETH', 'XRP']
   return (
     <div className="formComponent">
       <label htmlFor="valor">
         Valor:
         <input type="number" id="valor" data-testid="value-input" />
+      </label>
+      <label htmlFor="moeda">
+        Moeda:
+        <select id="moeda" data-testid="currency-input">
+  {moedas.map(moeda => <option data-testid={moeda} value={moeda}>{moeda}</option>)}
+        </select>
+      </label>
+      <label htmlFor="descricao">
+        Descrição:
+        <input type="text" id="descricao" data-testid="description-input" />
       </label>
     </div>
   );
