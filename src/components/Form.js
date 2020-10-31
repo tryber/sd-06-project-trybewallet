4f9b3e2b7907699408bf50a31e08810d8e71e471
ricardorosa-dev <ricardorosa@gmail.com>
2020-10-31 18:45:30 -0300

    esLint eu te amo.

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Form() {
   return (
-    <div class="formComponent">
-      <label for="valor">Valor: </label>
+    <div className="formComponent">
+      <label htmlFor="valor">Valor: </label>
       <input type="number" id="valor" data-testid="value-input" />
     </div>
-  )
+  );
 }
 
 export default Form;
