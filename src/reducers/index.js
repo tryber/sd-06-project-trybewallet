fb06aaaafaf1848044255693fdeb1f5d38a22118
DekoPossas <andrepossaster@gmail.com>
2020-10-31 13:32:17 -0300

    tentando criar o reduce, estou caminhando

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,17 @@
-// import user from './user';
-// import wallet from './wallet';
+import user from './user';
+import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const initialState = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: []
+  }
+}
+
+export default { user, wallet };
