f12df9ed0210f2f79568a9fa8a61e650cc4d7ca5
kissyla <kissylamottapereira@gmail.com>
2020-11-01 20:16:46 -0300

    erro eslint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,16 @@
 export const LOGIN = 'LOGIN';
-export const WALLET = 'WALLET';
+// export const WALLET = 'WALLET';
 
 export const loginAction = (email) => ({
   type: LOGIN,
   email,
 });
 
-export const currenciesAction = (currencies, expenses) => ({
-  type: WALLET,
-  currencies,
-  expenses,
-});
+// export const currenciesAction = (currencies, expenses) => ({
+//   type: WALLET,
+//   currencies,
+//   expenses,
+// });
 
 // action -> objeto com a propriedade type e mais qualquer outra chave que eu quiser
 // email->e-mail
