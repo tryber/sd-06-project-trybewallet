d951b7c4c86106c75b455a57e30c964366b91da7
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-11-01 01:11:18 -0300

    implementação básica do cabeçalho principal presente em todas as páginas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import logoTrybe from '../../img/trybe-logo.png';
+
+import './style.css';
 
 // Usando Emoji no REACT
 // [https://medium.com/@seanmcp/%EF%B8%8F-how-to-use-emojis-in-react-d23bbf608bf7]
@@ -8,16 +11,20 @@ class Header extends React.Component {
   render() {
     const { titulo } = this.props;
     return (
-      <>
+      <header className="header-contaier">
         <img src={ logoTrybe } alt="logo da Trybe" />
         <h1>
           <span role="img" aria-label="sacola de dinheiro">💰</span>
           {titulo}
           <span role="img" aria-label="caderninho de anotação">📔</span>
         </h1>
-      </>
+      </header>
     );
   }
 }
+
+Header.propTypes = {
+  titulo: PropTypes.string.isRequired,
+};
 
 export default Header;
