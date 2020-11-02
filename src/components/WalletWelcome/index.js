ef5467b77137252a8bc8a3f3ede1939fde8f072d
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-11-02 17:44:53 -0300

    implementa funcionalidade de exibir o valor da soma total das despesas já convertidas de acordo com a moeda escolhida
    destaque para a solução que eu não estava de jeito nenhum conseguindo me
    recordar como fazer, e que ficou clara, ao rever o plantão e o PR da
    colega Laís Gurgel.

---
+++ src/components/WalletWelcome/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class WalletWelcome extends React.Component {
+  render() {
+    const { email, expenses } = this.props;
+    return (
+      <div>
+        <p data-testid="email-field">
+          Olá,
+          <span>{ email }</span>
+          seja bem-vindx!
+          <span role="img" aria-label="coração verde">💚</span>
+          <span data-testid="header-currency-field">Total em BRL R$</span>
+          <p data-testid="total-field">
+            {/* como visto no PR da colega Laís Gurgel <3 */}
+            { expenses.reduce((previewValue, expense) => {
+              const { currency, exchangeRates, value } = expense;
+
+              const exchange = exchangeRates[currency].ask; // <3 <3
+              const finalBRLValue = exchange * value;
+
+              return previewValue + parseFloat(finalBRLValue);
+            }, 0).toFixed(2)}
+          </p>
+        </p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(WalletWelcome);
