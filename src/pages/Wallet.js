8fc005b402466b07713ad49676f52a1775fd067f
William Gomes <williamjog@hotmail.com>
2020-11-02 05:45:55 -0300

    Linter Fix 2.0

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -80,7 +80,7 @@ class Wallet extends React.Component {
           </span>
         </header>
         <form
-          onSubmit={ isEditing ? this.handleSubmitEditedExpense 
+          onSubmit={ isEditing ? this.handleSubmitEditedExpense
             : this.handleAddNewExpense }
         >
           <label htmlFor="value">
