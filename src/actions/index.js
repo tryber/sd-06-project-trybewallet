608a9aa794e48e9d71639acbb60d344bd5ba44f0
HCaetano <hrcgeo@gmail.com>
2020-11-02 19:36:17 -0300

    Fix unexpected space and missing semicolon on actions index file

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,3 +19,20 @@ export const currenciesThunk = () => async (dispatch) => {
 
   dispatch(currenciesFetch(currencies));
 };
+
+export const SAVE_EXPENSES = 'SAVE_EXPENSES';
+
+export const expensesAction = (expense) => ({
+  type: SAVE_EXPENSES,
+  expense,
+});
+
+export function fetchExchangeRates(expenseData) {
+  return async (dispatch) => {
+    const baseURL = 'https://economia.awesomeapi.com.br/json/all';
+    const response = await fetch(baseURL);
+    const currencies = await response.json();
+    const expense = { ...expenseData, exchangeRates: currencies };
+    dispatch(expensesAction(expense));
+  };
+}
