9b7865c43b83f6334d0aa42df5f971b7fb5a18f7
HCaetano <hrcgeo@gmail.com>
2020-11-02 19:35:02 -0300

    Fix maximum line length issue on wallet reducer

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { CURRENCIES_FETCH } from '../actions';
+import { CURRENCIES_FETCH, SAVE_EXPENSES } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -9,8 +9,14 @@ export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
   case CURRENCIES_FETCH:
     return {
-      ...state.currencies,
-      currencies: Object.keys(action.currencies).filter(currency => currency !== 'USDT'),
+      ...state,
+      currencies: Object.keys(action.currencies)
+        .filter((currency) => currency !== 'USDT'),
+    };
+  case SAVE_EXPENSES:
+    return {
+      ...state,
+      expenses: [...state.expenses, action.expense],
     };
   default:
     return state;
