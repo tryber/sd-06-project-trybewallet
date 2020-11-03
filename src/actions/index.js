9fca310e4cbe70af15752d8881ca9c4cb7fb2cd6
Macarena Rodriguez <macarenar697@gmail.com>
2020-11-03 17:12:53 -0300

    elemento que exiba o email da pessoa usuária que fez login.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
 export const EMAIL_LOGIN = 'EMAIL_LOGIN';
+export const TOTAL_FIELD = 'TOTAL_FIELD';
 
 export const addEmail = (email) => ({
   type: EMAIL_LOGIN,
   email,
 });
 
-export default addEmail;
+export const totalField = (total) => ({
+  type: TOTAL_FIELD,
+  total,
+});
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,11 +4,13 @@ import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    const { email } = this.props;
+    const { email, total } = this.props;
     return (
       <header>
         <div>Email: </div>
         <div data-testid="email-field">{ email }</div>
+        <div data-testid="total-field" value="0">{ total }</div>
+        <div data-testid="header-currency-field">BRL</div>
       </header>
     );
   }
@@ -16,10 +18,12 @@ class Wallet extends React.Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  total: state.wallet.total,
 });
 
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
+  total: PropTypes.string.isRequired,
 };
 
 export default connect(mapStateToProps, null)(Wallet);
