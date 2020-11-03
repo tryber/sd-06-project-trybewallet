d7758f62db63e0179a1c1ea25fdf6c809a949a3a
DekoPossas <andrepossaster@gmail.com>
2020-11-02 22:40:15 -0300

    criei um redux para wallet simples, objetivo coloca o site para funcionar

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  wallet: {
+    currency: [],
+    expenses: [],
+  }
+}
+
+function walletReducer (state = initialState, action) {
+  switch (action.type) {
+    case 'CHANGE':
+      return { ...state, ...action.payload}
+    default:
+      return state;
+  }
+}
+
+export default walletReducer;
