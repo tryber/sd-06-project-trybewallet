bc99fe4a6a5b5b81ec912140a2e67df85f76f43e
ilanribeiro <ilanribeiro@gmail.com>
2020-11-03 06:46:49 -0300

    feature: within src/index.js -> build reference including store as props

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import store from './store';
 import './index.css';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider>
-    <BrowserRouter store={store}>
+  <Provider store={ store }>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
