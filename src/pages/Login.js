f31cb7fe1c1147d344b64221823375b56b595cf5
DekoPossas <andrepossaster@gmail.com>
2020-11-02 23:12:28 -0300

    formatei o documento na experanca de melhorar o ESlint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,36 +4,37 @@ class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email:'',
-      password:'',
-    }
+      email: '',
+      password: '',
+    };
   }
 
   render() {
-    const {email, password} = this.props
-    return <div>
+    const { email, password } = this.props;
+    return (
       <div>
-        <label>
-          Login
-          <input 
-            type="email"
-            onChange={(e) => this.setState({ email:e.target.value })}
-            placeholder="Email"
-            data-testid="email-input"
-          />
-        </label>
-        <label>
-          <input
-            type="password"
-            data-testid="password-input"
-            placeholder="Password"
-            onChange={(e) => this.setState({ password: e.target.value })}
-          />
-        </label>
+        <div>
+          <label>
+            Login
+            <input
+              type="email"
+              onChange={(e) => this.setState({ email: e.target.value })}
+              placeholder="Email"
+              data-testid="email-input"
+            />
+          </label>
+          <label>
+            <input
+              type="password"
+              data-testid="password-input"
+              placeholder="Password"
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+          </label>
+        </div>
+        <button>Entrar</button>
       </div>
-      <button>Entrar</button>
-
-    </div>;
+    );
   }
 }
 
