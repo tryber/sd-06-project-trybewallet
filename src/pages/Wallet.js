cbbb6fadbc447b2ef95488f3d95e69c8083f1469
Igor Leão <igorleao.eng@gmail.com>
2020-11-03 18:17:04 -0300

    feat: requesit 3 wallet.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,30 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-  class Wallet extends Component {
-    render() {
-      const { userEmail } = this.props;
-      return (
-        <div>
-          <header>
-            <div data-testid="email-field">{userEmail}</div>
-          </header>
-          <h1>TrybeWallet</h1>
-          <section>
-            <span data-testid="total-field">0</span>
-            <span data-testid="header-currency-field">BRL</span>
-          </section>
-        </div>
-      );
-    }
+class Wallet extends Component {
+  render() {
+    const { userEmail } = this.props;
+    return (
+      <div>
+        <header>
+          <div data-testid="email-field">{userEmail}</div>
+        </header>
+        <h1>TrybeWallet</h1>
+        <section>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </section>
+      </div>
+    );
   }
+}
 
-  const mapStateToProps = (state) => ({
-    userEmail: state.user.email,
-  });
-  
-  Wallet.propTypes = {
-    userEmail: PropTypes.string.isRequired,
-  };
-  
-  export default connect(mapStateToProps)(Wallet);
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
