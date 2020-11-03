2e56c7ea184ac5216ccc95802d912e96bb4c0093
DekoPossas <andrepossaster@gmail.com>
2020-11-02 23:05:12 -0300

    criei um formulario

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email:'',
+      password:'',
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const {email, password} = this.props
+    return <div>
+      <div>
+        <label>
+          Login
+          <input 
+            type="email"
+            onChange={(e) => this.setState({ email:e.target.value })}
+            placeholder="Email"
+            data-testid="email-input"
+          />
+        </label>
+        <label>
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="Password"
+            onChange={(e) => this.setState({ password: e.target.value })}
+          />
+        </label>
+      </div>
+      <button>Entrar</button>
+
+    </div>;
   }
 }
 
