973b09365768cdcc6e44c4bf594864d051d91b07
Igor Leão <igorleao.eng@gmail.com>
2020-11-03 18:13:43 -0300

    feat: requesit 3 wallet.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-class Wallet extends React.Component {
-  render() {
-    return <div>TrybeWallet</div>;
+  class Wallet extends Component {
+    render() {
+      const { userEmail } = this.props;
+      return (
+        <div>
+          <header>
+            <div data-testid="email-field">{userEmail}</div>
+          </header>
+          <h1>TrybeWallet</h1>
+          <section>
+            <span data-testid="total-field">0</span>
+            <span data-testid="header-currency-field">BRL</span>
+          </section>
+        </div>
+      );
+    }
   }
-}
 
-export default Wallet;
+  const mapStateToProps = (state) => ({
+    userEmail: state.user.email,
+  });
+  
+  Wallet.propTypes = {
+    userEmail: PropTypes.string.isRequired,
+  };
+  
+  export default connect(mapStateToProps)(Wallet);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import user from './user';
-import wallet from './wallet';
+import wallet from '../pages/Wallet';
 
 const combiner = combineReducers({
   user,
