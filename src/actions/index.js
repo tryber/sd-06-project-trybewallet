7722ac99d2e9004d29ea58f447940a43cad23de9
ilanribeiro <ilanribeiro@gmail.com>
2020-11-03 06:43:18 -0300

    feature: within actions/index.js -> create two actions to add email and expenses

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 // Coloque aqui suas actions
+export const ADD_EMAIL = 'ADD_EMAIL';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+
+export const userEmail = (email) => ({
+  type: ADD_EMAIL,
+  email,
+});
+
+export const addExpense = (expense) => ({
+  type: ADD_EXPENSE,
+  expense,
+});
