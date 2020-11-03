07e9fd97862606f0ccb2289ed5bfe04258e6be75
Igor Leão <igorleao.eng@gmail.com>
2020-11-03 16:10:56 -0300

    feat: corretion eslint

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,9 @@ import thunk from 'redux-thunk';
 import combiner from '../reducers';
 
 // const reducer = () => {};
- const aux = createStore(combiner,
+const aux = createStore(combiner,
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ))
-  export  default aux;
+  ));
+export default aux;
