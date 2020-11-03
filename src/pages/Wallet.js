43ecaaf2cf59c927def493fc980e24660a449159
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-11-02 22:15:13 -0300

    inclui WalletHeader e Link para home

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
+import WalletHeader from '../components/WalletHeader';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header titulo="Oba! Vamos planejar!" />
+      <>
+        <Header titulo="Oba! Vamos planejar!" />
+        <WalletHeader />
+        <Link to="/">Voltar para home</Link>
+      </>
     );
   }
 }
