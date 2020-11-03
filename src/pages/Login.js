086b92160759bc37f42ed30decbb2073262f6cf8
ilanribeiro <ilanribeiro@gmail.com>
2020-11-03 06:48:43 -0300

    feature: within Login.js -> create page structure with necessary elements

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import Logo from './images/logo-trybe.png';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disableButton: true,
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login-container">
+        <img src={ Logo } alt="Logo Trybe" />
+        <input
+          data-testid="email-input"
+          type="email"
+          name="email"
+          id=""
+          placeholder="Digite aqui o seu email"
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          id=""
+          placeholder="Digite aqui a sua senha"
+        />
+        <button type="button">Entrar</button>
+      </div>);
   }
 }
 
