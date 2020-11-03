ff1a8b096ae9f1ba49b7ed98116eb77331d6dda0
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-11-02 22:33:12 -0300

    ironicamente não encontra o componente da página notFound remove a importação e o componente da aplicação

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
-import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
@@ -12,7 +11,6 @@ class App extends React.Component {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/carteira" component={ Wallet } />
-        <Route component={ NotFound } />
       </Switch>
     );
   }
