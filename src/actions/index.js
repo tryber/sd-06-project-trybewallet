823d2c280a5d6f8f9bc5f786dd6a1f2e942c16c3
Aloizio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-11-03 15:19:49 -0300

    ainda não ta funcionando

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,6 @@
-// import redux from 'redux';
-
-// redux.dispatch({
-//   type: 'ALGO',
-//   parametro: 'algo',
-// });
-
 export const LOGIN = 'LOGIN';
 
 export const login = (email) => ({
-  type: LOGIN,
+  type: 'LOGIN',
   email,
 });
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,65 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { login } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
+    const { email, password } = this.state;
+    const { login } = this.props;
+    const magic = 6;
+    const activeButton = email.length > 0 && password.length > magic;
+
     return (
       <div>
         <label htmlFor="email">
           Email:
-          <input type="email" data-testid="email-input" />
+          <input
+            type="email"
+            value={ email }
+            onChange={ (event) => this.setState({ email: event.target.value }) }
+            required
+            placeholder="alguem@alguem.com"
+            data-testid="email-input"
+          />
         </label>
         <br />
         <label htmlFor="password">
           Senha:
-          <input type="password" data-testid="password-input" />
+          <input
+            type="password"
+            value={ password }
+            onChange={ (event) => this.setState({ password: event.target.value }) }
+            required
+            placeholder="123456...¯\_(ツ)_/¯"
+            minLength="6"
+            data-testid="password-input"
+          />
         </label>
         <br />
-        <button type="button">Entrar</button>
+        <Link to="/carteira ">
+          <button
+            disabled={ !activeButton }
+            type="button"
+            onClick={ () => login(email) }
+          >
+            Entrar
+          </button>
+        </Link>
       </div>);
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: (email) => dispatch(login(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
