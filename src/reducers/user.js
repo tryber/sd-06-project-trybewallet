d4581076552d2ee02a1ab26d41e1e4a7d90f03c0
ilanribeiro <ilanribeiro@gmail.com>
2020-11-03 06:51:39 -0300

    feature: within Reducers -> builded user and wallet reducers. combine the two as one within reducers/index.js

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {};
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case '':
+    return {};
+  default:
+    return state;
+  }
+}
+
+export default userReducer;
