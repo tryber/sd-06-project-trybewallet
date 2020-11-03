8a3a8b7c75c43d5762f0efb8674c824b2f733212
Igor Leão <igorleao.eng@gmail.com>
2020-11-03 16:03:26 -0300

    wallet Project

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, createStore, compose } from 'react-redux';
+import thunk from 'redux-thunk';
+import combiner from '../reducers';
+
+// const reducer = () => {};
+ const aux = createStore(combiner,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ))
+  export  default aux;
