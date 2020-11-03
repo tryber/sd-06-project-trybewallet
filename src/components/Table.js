96fddf81652aac016fa536855dfe7f892e0cb88c
danieleperse <danieleperse3@gmail.com>
2020-11-03 19:22:08 -0300

    tentando implementar botao delete

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,24 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Table extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.removeItem = this.removeItem.bind(this);
+  // }
+
+  // removeItem(id) {
+  //   const { expenses } = this.props;
+
+  //   const newArray = expenses.filter((expense) => {
+  //     return expense.id !== id
+  //   })
+  //   newArray = [...]
+    // expenses.filter(() => {
+    //   //const nome = `${(this).attr('name')}`;
+    //   const nome = document.getElementsByName("button")
+    //   return id !== nome
+  // }
+
   render() {
     const headerTable = ['Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
       'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
@@ -16,13 +34,13 @@ class Table extends React.Component {
         </thead>
         <thead>
           { expenses.map((expense) => {
-            const { currency, exchangeRates } = expense;
-            const valorDespesa = expense.value;
+            const { currency, exchangeRates, id, value } = expense;
+            const valorDespesa = value;
             const nomeMoeda = exchangeRates[currency].name;
             const nomeMoedaConversao = 'Real';
             const cambio = parseFloat(exchangeRates[currency].ask);
             return (
-              <tr key={ expense.id }>
+              <tr key={ id }>
                 <td>{ expense.description }</td>
                 <td>{ expense.tag }</td>
                 <td>{ expense.method }</td>
@@ -31,6 +49,13 @@ class Table extends React.Component {
                 <td>{ cambio.toFixed(2) }</td>
                 <td>{ (valorDespesa * cambio).toFixed(2) }</td>
                 <td>{ nomeMoedaConversao }</td>
+                <td>
+                  <button
+                    data-testid="delete-btn"
+                    name={ id }
+                    // onClick={ this.removeItem(id) }
+                  >Delete</button>
+                </td>
               </tr>
             );
           })}
