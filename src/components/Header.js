4a2c2aa8ed1fdcff50e086364f8f9cd3ea22bc52
danieleperse <danieleperse3@gmail.com>
2020-11-03 08:47:57 -0300

    acertar valores header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,26 @@ import './header.css';
 import logo from '../images/logo.png';
 
 class Header extends React.Component {
-  // totalExpenses(listOfExpense) {
-  //   if (listOfExpense != null && listOfExpense.length === 0) {
-  //     return 0;
-  //   }
-  //   let sumExpenses = 0;
-  //   listOfExpense.forEach((expense) => {
-  //     sumExpenses += parseFloat(expense.value);
-  //   });
-  //   return sumExpenses;
-  // }
+  constructor() {
+    super();
+    this.totalExpenses = this.totalExpenses.bind(this);
+  }
 
-  //  O resultado da soma deve ser o valor multiplicado pela cotação feita na requisiçao da Api. A soma esta dando 30 quando deveria dar 187.12
-
-  // { this.totalExpenses(wallet.expenses) }
+  totalExpenses(listOfExpense) {
+    console.log('chemai a função')
+    if (listOfExpense != null && listOfExpense.length === 0) {
+      return 0;
+    }
+    let sumExpenses = 0;
+    listOfExpense.forEach((expense) => {
+      sumExpenses += parseFloat(expense.value);
+    });
+    return sumExpenses;
+  }
 
   render() {
-    const { email, wallet } = this.props;
+    const { email, expenses } = this.props;
+    console.log(expenses.length)
     return (
       <header className="form-header">
         <img src={ logo } alt="logo-trybe" />
@@ -29,7 +32,7 @@ class Header extends React.Component {
         <span data-testid="email-field">{ email }</span>
         <span>Despesa Total:</span>
         <span data-testid="total-field">
-          0
+          {this.totalExpenses(expenses)}
         </span>
         <span>Câmbio utilizado:</span>
         <span data-testid="header-currency-field">BRL</span>
@@ -40,15 +43,15 @@ class Header extends React.Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
-  wallet: state.wallet,
+  expenses: state.wallet.expenses,
 });
 
-Header.propTypes = {
-  email: PropTypes.string.isRequired,
-  wallet: PropTypes.shape({
-    expenses: PropTypes.arrayOf(PropTypes.object),
-  }).isRequired,
-};
+// Header.propTypes = {
+//   email: PropTypes.string.isRequired,
+//   wallet: PropTypes.shape({
+//     expenses: PropTypes.arrayOf(PropTypes.object),
+//   }).isRequired,
+// };
 
 export default connect(
   mapStateToProps,
