65515b6302d6a3575b6d837adbf717e9fa484cb1
Anderson <andersoni.trybe@gmail.com>
2020-11-03 21:07:03 -0300

    Actions da Wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,2 +1,19 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
- import {} from '../actions';
+ import { ADD_EXPENSES, RECEIVE_CURRENCY_OK } from '../actions';
+
+ const INITIAL_STATE = {
+     currencies: [],
+     expenses: [],
+ };
+
+ export default function walletTransaction(state = INITIAL_STATE, action) {
+    switch (action.type) {
+    case RECEIVE_CURRENCY_OK:
+      return { ...state, currencies: action.currencies };
+    case ADD_EXPENSES:
+      return { ...state, expenses: [...state.expenses, action.expenses] };
+    default:
+      return state;
+    }
+  }
+  
