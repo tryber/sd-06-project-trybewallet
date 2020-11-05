2ed6650c01cfb97468f1cf91d4f7fda6e7745dde
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-11-05 14:18:52 -0300

    Linha 90: Unexpected tab character.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -87,7 +87,7 @@ class Wallet extends React.Component {
     const { currency, value } = this.state;
     await fetch();
     let bid = 0;
-    
+
     Object.keys(rates).forEach((item) => {
       if (currency === item) {
         bid = rates[item].ask;
