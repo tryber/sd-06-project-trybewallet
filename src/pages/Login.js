a660b45dbe53d5c46e5748b3d65025fb388ddef1
RenatoCostaDev <renatotrybe@gmail.com>
2020-11-06 11:02:05 -0300

    feat: input do email,input do password e button (Entrar)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="email"
+            data-testid="email-input"
+            placeholder="Login"
+            value=""
+          />
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            placeholder="Password"
+            value=""
+          />
+          <button
+            type="button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
