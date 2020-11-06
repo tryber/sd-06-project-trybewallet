a679bec98a39da4d4111e9d24c2ba81ee3c411fa
rafMewtwo <ponciano.raf@gmail.com>
2020-11-06 00:27:09 -0300

    testttttfinalsaast

--- src/components/SelectMoeda.js
+++ src/components/SelectMoeda.js
@@ -44,12 +44,12 @@ class SelectMoeda extends React.Component {
         >
           { currencys.map((currencyZ) => {
             const opt = <option
-            value={ currencyZ }
-            data-testid={ currencyZ }
-            key={ currencyZ }
-          >
-            { currencyZ }
-          </option>
+              value={ currencyZ }
+              data-testid={ currencyZ }
+              key={ currencyZ }
+            >
+              { currencyZ }
+            </option>
             return opt;
           })}
         </select>
