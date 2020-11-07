a7e976e33ed946f82e7a89db383a243550fdffd0
Micaeli Teixeira <luime6@gmail.com>
2020-11-07 17:38:11 -0300

    change of code organization in action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,34 @@
 export const EMAIL = 'EMAIL';
-
-export const salveUserEmail = (value) => ({
+export const salveUserEmail = (email) => ({
   type: EMAIL,
-  value,
+  email,
 });
+
+export const CURRENCY = 'CURRENCY';
+export const currencyAction = (currency) => ({
+  type: CURRENCY,
+  currency,
+});
+
+export const EXPENSES = 'EXPENSES';
+export const addExpense = (expenses) => ({
+  type: EXPENSES,
+  expenses,
+});
+
+export function newCurrency(expense) {
+  return async (dispatch) => {
+    const getCurrencies = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const currencies = await getCurrencies.json();
+    const allExpenses = { ...expense, exchangeRates: currencies };
+    dispatch(addExpense(allExpenses));
+  };
+}
+
+export function fetchCurrency() {
+  return (dispatch) => {
+    fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((response) => dispatch(currencyAction(response)));
+  };
+}
