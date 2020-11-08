6199ccce34391a3d956cc8c0939ddf446af8f689
Cássio Rubens <cassiorubensadv@gmail.com>
2020-11-08 15:11:30 -0300

    FIX: mock API

--- src/services/currencyApi.js
+++ src/services/currencyApi.js
@@ -1,13 +1,13 @@
-// import { response } from '../tests/mockData';
-
-function fetchApiData() {
-  const ENDPOINT = 'https://economia.awesomeapi.com.br/json/all';
-  return fetch(ENDPOINT)
-    .then((response) => response.json());
-}
+import { response } from '../tests/mockData';
 
 // function fetchApiData() {
-//   return Promise.resolve(response);
+//   const ENDPOINT = 'https://economia.awesomeapi.com.br/json/all';
+//   return fetch(ENDPOINT)
+//     .then((response) => response.json());
 // }
+
+function fetchApiData() {
+  return Promise.resolve(response);
+}
 
 export default fetchApiData;
