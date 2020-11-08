d93821c59f662bbb09cdd0a150bb9dfe9bec1095
Matheuscara <matheuscara2000@gmail.com>
2020-11-08 14:52:06 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import Wallet from './pages/Wallet';
+import wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/carteira" component={ Wallet } />
+      <Route path="/carteira" component={ wallet } />
     </Switch>
   );
 }
