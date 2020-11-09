f7a48de3b37512ec6e50d77f67c56ba355f16fd5
Daniel Jesus <dev.danieljs@gmail.com>
2020-11-08 22:28:55 -0300

    feat(wallet): config Wallet page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Login } from './pages';
+import { Login, Wallet } from './pages';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
       </Switch>
     </div>
   );
