8bf976baeb4ed47070b5e3d18fbb82d1079f84bd
Stefano <stefano.branz@gmail.com>
2020-11-10 13:46:10 -0300

    Requirement 05 push 01

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import AddExpensesForm from '../components/AddExpensesForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <AddExpensesForm />
+        <Table />
       </div>
     );
   }
