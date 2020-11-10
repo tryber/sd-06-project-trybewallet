a851234bb470cc1c5437549c4a563ff800d50e7d
LughWalle <civalfran1@gmail.com>
2020-11-10 07:46:28 -0300

    lint fixed

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { propTypes } from 'prop-type'
 
 class Wallet extends React.Component {
   render() {
-    const { email, currency } = this.props; 
-    return <div>
-    TrybeWallet
-    <div data-testid="email-field">{email}</div>
-    <div data-testid="total-field" value="0"></div>
-    <div data-testid="header-currency-field">{currency}</div>
-    </div>;
+    const { email, currency, expense } = this.props; 
+    return (
+      <div>
+        TrybeWallet
+        <div data-testid="email-field">{email}</div>
+        <div data-testid="total-field" value="0">{expense}</div>
+        <div data-testid="header-currency-field">{currency}</div>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = store => ({
+const mapStateToProps = (store) => ({
   email: store.user.email,
   currency: store.wallet.currencies,
   expense: store.wallet.expenses,
 });
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+  expense: PropTypes.number.isRequired,
+}
 
 export default connect(mapStateToProps)(Wallet);
