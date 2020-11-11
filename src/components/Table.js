32c5800a490f350eccf90e4b7f97b5cdeaa691ae
Paulo Frazão <paulo4187@gmail.com>
2020-11-10 21:12:54 -0300

    Req: 06

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Table extends Component {
+  componentDidUpdate() {
+    const { expenses } = this.props;
+    return expenses;
+  }
+
+  handleUpdate(exp) {
+    const { expenses } = this.props;
+    expenses.splice(exp, 1);
+    this.setState({
+      test: true,
+    });
+  }
+
   render() {
     const { expenses } = this.props;
-    console.log(expenses);
     return (
       <div>
         { expenses.length > 0
@@ -19,6 +31,7 @@ class Table extends Component {
                 <td>Câmbio utilizado</td>
                 <td>Valor convertido</td>
                 <td>Moeda de conversão</td>
+                <td>Editar/Excluir</td>
 
               </tr>
               {expenses.map((exp, index) => (
@@ -32,18 +45,22 @@ class Table extends Component {
                   <td>{Math.round(exp.exchangeRates[exp.currency].ask * 100) / 100}</td>
                   <td>{(exp.exchangeRates[exp.currency].ask * exp.value).toFixed(2)}</td>
                   <td>Real</td>
+                  <td>
+                    <button
+                      data-testid="delete-btn"
+                      type="button"
+                      onClick={ () => this.handleUpdate(exp.id) }
+                    >
+                      Excluir
+                    </button>
+                  </td>
                 </tr>
 
               ))}
             </table>
 
           ) : <p>Vazio</p>}
-        <button
-          type="button"
-          disabled={ expenses.length === 0 }
-        >
-          Editar/Excluir
-        </button>
+
       </div>
     );
   }
