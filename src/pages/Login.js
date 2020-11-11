2db81d7477c023cb24ae99fedc724599887ea994
vapatty6848 <vapatty@gmail.com>
2020-11-11 13:18:06 -0300

    corrigindo Lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { login } from '../actions';
 
 class Login extends React.Component {
