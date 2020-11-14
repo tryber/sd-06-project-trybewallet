b1fae75b02af74d2b880d44f62191219b18e933b
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-11-14 14:50:35 -0300

    PROJETO TRYBEWALLET

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/carteira" component={Wallet} />
+      </Switch>
+    );
+  }
 }
+// function App() {
+//   return <div>Hello, TrybeWallet!</div>,
+// }
 
 export default App;
+
+// referencias
+// https://www.codigofonte.com.br/codigos/validar-e-mail-via-javascript
+// 
