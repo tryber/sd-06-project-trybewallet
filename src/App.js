aba3ad0c7bf97ce3876bc5ca7994277785b03d1e
Pedro Eduardo <pedroedu77@gmail.com>
2020-11-16 15:12:45 -0300

    all requisits and lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
-// import Wallet from './pages/Wallet';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
       <Switch>
+        <Route path="/carteira" component={ Wallet } />
         <Route exact path="/" component={ Login } />
-        <Route exact path="/carteira" component={ Carteira } />
       </Switch>
     </div>
   );
