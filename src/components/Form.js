93d5755430615e1550e8f4f65103fa96d1f21640
RenatoCostaDev <renatotrybe@gmail.com>
2020-11-17 14:37:01 -0300

    feat: component Form.js para requisito-1

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Formulário</p>
+      </div>
+    );
+  }
+}
+
+export default Form;
