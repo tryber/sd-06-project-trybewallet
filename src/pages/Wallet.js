c724fcc6c561d4360ad297b6205aa2811eb617f9
Pedro Eduardo <pedroedu77@gmail.com>
2020-11-17 13:39:53 -0300

    All requisits

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -232,7 +232,6 @@ Wallet.propTypes = {
   expenses: PropTypes.arrayOf(PropTypes.shape()).isRequired,
   editData: PropTypes.func.isRequired,
   addData: PropTypes.func.isRequired,
-  rates: PropTypes.arrayOf(PropTypes.string).isRequired,
   total: PropTypes.number,
   email: PropTypes.string.isRequired,
 };
