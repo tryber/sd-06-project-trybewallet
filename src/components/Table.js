fa6c9a2f9e8b161e9b13fd1a3a96a4b6899e5008
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-11-18 19:00:02 -0300

    teste 03

--- src/components/Table.js
+++ src/components/Table.js
@@ -17,7 +17,7 @@ class Table extends Component {
       id,
       description,
       method,
-      tag,
+      Tag,
       value,
       currency,
       exchangeRates,
@@ -32,7 +32,7 @@ class Table extends Component {
     return (
       <tr id={ id }>
         <td>{description}</td>
-        <td>{tag}</td>
+        <td>{Tag}</td>
         <td>{method}</td>
         <td>{value}</td>
         <td>{name}</td>
