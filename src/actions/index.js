ef45811a7049038af03fe6be63749ad399a8759c
Danilo <dansantosalfa@gmail.com>
2020-11-28 19:03:50 -0300

    Criando actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
+export const EMAIL_LOGIN = 'EMAIL_LOGIN';
+export const TOTAL_FIELD = 'TOTAL_FIELD';
 
+export const addEmail = (email) => ({
+  type: EMAIL_LOGIN,
+  email,
+});
+
+export const totalField = (total) => ({
+  type: TOTAL_FIELD,
+  total,
+});
