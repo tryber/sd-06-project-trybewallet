50f84934cf88c6342c0ee16c89ed27e14535eec4
RenatoCostaDev <renatotrybe@gmail.com>
2020-11-28 18:17:40 -0300

    feat: Table.jsx and thead

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-// , devTools
-const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), devTools));
 export default store;
