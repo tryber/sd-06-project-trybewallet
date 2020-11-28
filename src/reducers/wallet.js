d26ede2e984873143b7f99503d84c6af3fdc5d80
Danilo <dansantosalfa@gmail.com>
2020-11-28 19:00:32 -0300

    Mexendo com reducer, Pages, index e criando store

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { TOTAL_FIELD } from '../actions';
+
+const INITIAL_STATE = {
+  total: 0,
+};
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case TOTAL_FIELD:
+    return {
+      ...state, total: action.total,
+    };
+  default:
+    return state;
+  }
+}
