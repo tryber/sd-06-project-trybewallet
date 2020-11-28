6c3dda67695e0015ae7ed385422e3ff161f3a78a
Danilo <dansantosalfa@gmail.com>
2020-11-28 18:33:12 -0300

    Teste do login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      login: '',
+      password: '',
+      logged: true,
+    };
+    this.testVerifc = this.testVerifc.bind(this);
+  }
+
+  testVerifc() {
+    const { username, password } = this.state;
+    const verificated = 5;
+    const re = /^\w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$/;
+    if (re.test(String(username).toLowerCase()) && password.length >= verificated) {
+      this.setState({
+        logged: false,
+      });
+    } else {
+      this.setState({
+        logged: true,
+      });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { login, password, logged } = this.state;
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            data-testid="email-input"
+            placeholder="Login"
+            value={ login }
+            required="required"
+          />
+          <input
+            type="password"
+            data-testid="email-input"
+            placeholder="Password"
+            minlength="6"
+            value={ password }
+            required="required"
+          />
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ logged }
+            >
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </div>
+    );
   }
 }
 
