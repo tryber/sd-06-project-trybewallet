fb760f808ffb80228bb771c1f532dc159146b9e0
Danilo <dansantosalfa@gmail.com>
2020-11-29 14:23:38 -0300

    Requisitos de 1 - 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/header';
 
-class Wallet extends React.Component {
+export default class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <Header />
+    );
   }
 }
-
-export default Wallet;
