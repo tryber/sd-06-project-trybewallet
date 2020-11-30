181d4522d872d4f6a76182f3b5f4f64d5685b1ce
DekoPossas <andrepossaster@gmail.com>
2020-11-30 10:05:46 -0300

    criei toda a estrutura do header

--- src/componente/Form.js
+++ src/componente/Form.js
@@ -22,6 +22,7 @@ class Form extends React.Component {
           Cambio
           <select
             data-testid="currency-input"
+            name="cambio"
           >
             <option value="usd" data-testid="USD">USD</option>
             <option value="cad" data-testid="CAD">CAD</option>
@@ -39,6 +40,28 @@ class Form extends React.Component {
             <option value="xrp" data-testid="XRP">XRP</option>
           </select>
         </label>
+        <label htmlFor="method">
+          Método de pagamento
+          <select name="method" data-testid="method-input">
+            <option value="dinheiro">Dinheiro</option>
+            <option value="credito">Cartão de crédito</option>
+            <option value="debito">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="category">
+          Categoria para Dispesa
+          <select data-testid="tag-input" name="category">
+            <option value="alimentacao">Alimentação</option>
+            <option value="lazer">Lazer</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="transporte">Transporte</option>
+            <option value="saude">Saúde</option>
+          </select>
+        </label>
+        <label htmlFor="submit">
+          <button type="button">Adicionar Dispesa</button>
+        </label>
+        <label htmlFor="total-field" data-testid="total-field">Total: 0</label>
       </form>
     )
   }
