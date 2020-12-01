fde7a77439acc02da67e616601977b8ad6cd0b92
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-11-30 21:57:13 -0300

    continuando a corrigir

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
+import propType from 'prop-types';
 import '../css/header.css';
 
-
 class Header extends Component {
-
   render() {
     const { email, total } = this.props;
-    return(
+    const moedaAtual = 'BRL';
+    return (
       <div className="walletHeader">
         <div className="user">
           <p className="text-name">Usuário</p>
-          <p className="email" date-testid="email-field">{ email } </p>
+          <p className="email" date-testid="email-field">{ email }</p>
         </div>
         <div className="title">
           <h1 className="text-title">Controle de Despesas.</h1>
         </div>
         <div className="info">
-          <p className="cambio" tada-testid="header-currency-field">"BRL"</p>
+          <p className="cambio" tada-testid="header-currency-field">{ moedaAtual }</p>
           <p className="valor_total" data-testid="total-field">{ total }</p>
         </div>
       </div>
 
     );
-  };
+  }
 }
+
+Header.propTypes = {
+  email: propType.string.isRequired,
+  total: propType.number.isRequired,
+};
 
 export default Header;
