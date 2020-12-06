cf6448451559c3159e3f1e51a63330cf39bcc25b
lucasvr39 <lucasveras1994@gmail.com>
2020-12-06 02:02:24 -0300

    implement req 2

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import store from '../store/index';
 
+// get user email from my state - don't forget to dispatch before try to get something in global state
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+// just to check if i got the correct value in state
+store.subscribe(() => {
+  console.log('state\n', store.getState());
+});
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <h1>
+          TrybeWallet
+        </h1>
+        <h4>
+          {email}
+        </h4>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.shape.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
